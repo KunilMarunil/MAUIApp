687134329bac0249872853bca6df363d95944222
ramadhanu <ramadhanu040@gmail.com>
2024-02-28 07:15:30 +0700

    First Commit

---
+++ Testing/obj/Release/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		mono.android.Runtime.register ("Testing.MainApplication, Testing, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64fa0ef4535af16703.MainApplication.class, crc64fa0ef4535af16703.MainApplication.__md_methods);
+		
+	}
+}
